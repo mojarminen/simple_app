2bc00149840fb0b596f9e7bce7b3b84bc9f5ea71
Mikko Arminen <mikko.arminen@uta.fi>
2016-08-27 09:20:52 +0300

    data added, refactoring

--- history.py
+++ history.py
@@ -25,6 +25,79 @@ def get_matches(team=None, league=None, season=None, start=None, end=None, cance
     return db.get_matches(team_id=team_id, start=start, end=end, league_id=league_id, season_id=season_id, cancelled=cancelled, awarded=awarded)
 
 
+def print_match_preview(home_team, away_team, season):
+    
+    print 'HOME TEAM:', home_team
+    print
+    
+    if '-' in season:
+        for s in range(int(season.split('-')[0]), int(season.split('-')[0])-4, -1):
+            s_str = str(s) + '-' + str(s+1)
+            print '\thome percentages', s_str, ':', get_home_match_percentages_of_team(home_team, season=s_str)
+        print
+        for s in range(int(season.split('-')[0]), int(season.split('-')[0])-4, -1):
+            s_str = str(s) + '-' + str(s+1)
+            print '\tmatch percentages', s_str, ':', get_match_percentages_of_team(home_team, season=s_str)
+        print
+
+        print 'AWAY TEAM:', away_team
+        print
+        for s in range(int(season.split('-')[0]), int(season.split('-')[0])-4, -1):
+            s_str = str(s) + '-' + str(s+1)
+            print '\taway percentages', s_str, ':', get_away_match_percentages_of_team(away_team, season=s_str)
+        print
+        for s in range(int(season.split('-')[0]), int(season.split('-')[0])-4, -1):
+            s_str = str(s) + '-' + str(s+1)
+            print '\tmatch percentages', s_str, ':', get_match_percentages_of_team(away_team, season=s_str)
+        print
+        
+        s = str(int(season.split('-')[0])-1) + '-' + str(int(season.split('-')[1])-1)
+    else:
+        for s in range(int(season), int(season)-4, -1):
+            s_str = str(s)
+            print '\thome percentages', s_str, ':', get_home_match_percentages_of_team(home_team, season=s_str)
+        print
+        for s in range(int(season), int(season)-4, -1):
+            s_str = str(s)
+            print '\tmatch percentages', s_str, ':', get_match_percentages_of_team(home_team, season=s_str)
+        print
+
+        print 'AWAY TEAM:', away_team
+        print
+        for s in range(int(season), int(season)-4, -1):
+            s_str = str(s)
+            print '\taway percentages', s_str, ':', get_away_match_percentages_of_team(away_team, season=s_str)
+        print
+        for s in range(int(season), int(season)-4, -1):
+            s_str = str(s)
+            print '\tmatch percentages', s_str, ':', get_match_percentages_of_team(away_team, season=s_str)
+        print
+        
+        s = season
+        
+
+    home_home_percentages = get_home_match_percentages_of_team(home_team, season=s)
+    home_match_percentages = get_match_percentages_of_team(home_team, season=s)
+    away_away_percentages = get_away_match_percentages_of_team(away_team, season=s)
+    away_match_percentages = get_match_percentages_of_team(away_team, season=s)
+    
+    general_1X2 = get_1X2_percentages(season=s)
+    print 'GENERAL 1X2:', general_1X2
+    print
+        
+    print 'ESTIMATED PROBABILITIES:', 
+    home_win_p = (home_home_percentages[0] + home_match_percentages[0] + away_away_percentages[2] + away_match_percentages[2] + general_1X2[0])*100./5.
+    print home_win_p,
+    print '-',
+    draw_p = (home_home_percentages[1] + home_match_percentages[1] + away_away_percentages[1] + away_match_percentages[1] + general_1X2[1])*100./5.
+    print draw_p,
+    print '-',
+    away_win_p = (home_home_percentages[2] + home_match_percentages[2] + away_away_percentages[0] + away_match_percentages[0] + general_1X2[2])*100./5. 
+    print away_win_p
+    
+    print 'ODD LIMITS:', 100./home_win_p, '-', 100./draw_p, '-', 100./away_win_p
+
+
 def get_n_previous_matches_of_team(team, date, count, league=None, season=None):
 
     team_id = db.get_team_id(team)
